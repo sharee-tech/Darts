31b8f63c09fcff3a7f16357d5311db190fd24c02
sharee-tech <84551254+sharee-tech@users.noreply.github.com>
2024-04-18 19:30:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import math
+
+def score(x, y):
+  r = math.sqrt(x**2 + y**2)
+  if r < 1:
+    return 10
+  elif r < 5:
+    return 5
+  elif r < 10:
+    return 1
+  else:
+    return 0
+
+
+print(score(1, 1))
